e78102fc225338ae42fb02dcb6abb8c520f32423
karim magdy <kareem147j@gmail.com>
2026-09-14 12:39:18 +0300

    add tawk.io

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -114,6 +114,21 @@ function RootShell({ children }: { children: ReactNode }) {
       <body>
         {children}
         <Scripts />
+        {/* Start of Tawk.to Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6aa7bfcc2c15d634461470c8/1k2fkae0f';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`,
+          }}
+        />
+        {/* End of Tawk.to Script */}
       </body>
     </html>
   );
