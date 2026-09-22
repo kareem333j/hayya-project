b9274cba645e9b5c2a8646568ace345828112bfa
karim magdy <kareem147j@gmail.com>
2026-09-22 21:25:23 +0300

    update

--- supabase/functions/send-inquiry/index.ts
+++ supabase/functions/send-inquiry/index.ts
@@ -5,7 +5,7 @@ declare var Deno: {
 
 const RESEND_API_KEY = Deno.env.get("RESEND_API_KEY");
 // const TO_EMAIL = "rezk@hayya-eg.com";
-const TO_EMAIL = "kareem147j@gmail.com";
+const TO_EMAIL = "rezk@hayya-eg.com";
 const FROM_EMAIL = "inquiries@hayya-eg.com"; // must be a verified domain in Resend
 
 const corsHeaders = {
